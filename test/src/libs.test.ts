c6952577920f7891ccf5bfad9793a09b968a63f6
romina <rominacannizzaro@gmail.com>
2023-10-09 21:18:11 +0300

    test: add getMainImage test + add testArticle

--- test/src/libs.test.ts
+++ test/src/libs.test.ts
@@ -1,23 +1,39 @@
-import * as libs from '../../src/libs';
+import * as libs from "../../src/libs";
+import { article } from "./testArticle";
 
-describe('getImageElement', () => {
-  it('should return image element', () => {
-    const imageUrl = 'https://www.seiska.fi/image/image_gallery?uuid=MTk3NzI4MzE&groupId=10221822&t=1609247778000';
-    const caption = 'caption';
+describe("getImageElement", () => {
+  it("should return image element", () => {
+    const imageUrl =
+      "https://www.seiska.fi/image/image_gallery?uuid=MTk3NzI4MzE&groupId=10221822&t=1609247778000";
+    const caption = "caption";
     const imageElement = libs.getImageElement(imageUrl, caption);
-    expect(imageElement).toEqual(`<figure class="pp-media"><img class="pp-media__image" alt="${caption}" src="${imageUrl}"><figcaption class="pp-media__caption">${caption}</figcaption></figure>`);
+    expect(imageElement).toEqual(
+      `<figure class="pp-media"><img class="pp-media__image" alt="${caption}" src="${imageUrl}"><figcaption class="pp-media__caption">${caption}</figcaption></figure>`
+    );
   });
 });
 
-describe('getCropParams', () => {
-  it('should return crop params', () => {
+describe("getCropParams", () => {
+  it("should return crop params", () => {
     const cropParams = {
       cropw: 100,
       croph: 100,
       x: 10,
       y: 10,
-    }
+    };
     const params = libs.getCropParams(cropParams);
-    expect(params).toEqual('x=10&y=10&cropw=100&croph=100&');
+    expect(params).toEqual("x=10&y=10&cropw=100&croph=100&");
+  });
+});
+
+describe("getMainImage", () => {
+  it("should return main image", () => {
+    const id =
+      article?.children?.articleHeader?.children?.image?.attribute
+        ?.instanceof_id;
+    const mainImageElement = libs.getMainImage(article);
+    expect(mainImageElement).toContain(
+      `https://image.seiska.fi/${id}.jpg?width=710&height=400&`
+    );
   });
 });
