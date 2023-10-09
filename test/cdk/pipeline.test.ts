fe389497fcd4a6a99dae783c8e7008318b0b468d
Phu <ngp130895@gmail.com>
2023-10-09 13:46:06 +0300

    test: renamed test

--- test/cdk/pipeline.test.ts
+++ test/cdk/pipeline.test.ts
@@ -1,14 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import * as Cdk from "../../lib/PipelineStack";
-// create random test
-describe("Stack", () => {
+
+describe("Pipeline Stack", () => {
   const app = new cdk.App();
   const stack = new Cdk.PipelineStack(app, "MyTestStack");
   const template = Template.fromStack(stack);
-  it("should have 1 resource of type AWS::CodePipeline::Pipeline", () => {
-    template.resourceCountIs("AWS::CodePipeline::Pipeline", 1);
-  });
 
   it("should have a pipeline with 4 stages", () => {
     template.hasResourceProperties("AWS::CodePipeline::Pipeline", {
