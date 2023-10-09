c92c9ee9ef48cd656a00269cd4c411c786f9f277
Phu <ngp130895@gmail.com>
2023-10-09 12:10:21 +0300

    test: moved test case to correct container

--- test/cdk/stack.test.ts
+++ test/cdk/stack.test.ts
@@ -3,8 +3,10 @@ import { Template } from 'aws-cdk-lib/assertions';
 import * as Cdk from '../../lib/PipelineStack';
 // create random test 
 describe('Stack', () => {
-  const app = new cdk.App();
-  const stack = new Cdk.PipelineStack(app, 'MyTestStack');
-  const template = Template.fromStack(stack);
-  template.resourceCountIs('AWS::CodePipeline::Pipeline', 1);
+  it('should have 1 resource of type AWS::CodePipeline::Pipeline', () => {
+    const app = new cdk.App();
+    const stack = new Cdk.PipelineStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::CodePipeline::Pipeline', 1);
+  });
 });
